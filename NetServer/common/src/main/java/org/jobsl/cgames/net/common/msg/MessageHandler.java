ea63e46bc7c0b9336d6cd8be827be86277f17443
JobsLeeGeek <jobs.lee@outlook.com>
2021-01-28 21:40:45 +0800

    删除netserver多余文件

--- NetServer/common/src/main/java/org/jobsl/cgames/net/common/msg/MessageHandler.java
+++
@@ -1,9 +0,0 @@
-package org.jobsl.cgames.net.common.msg;
-
-import io.netty.channel.Channel;
-
-public interface MessageHandler {
-    void send(Channel channel, Message msg);
-
-    void rec(Message msg);
-}
